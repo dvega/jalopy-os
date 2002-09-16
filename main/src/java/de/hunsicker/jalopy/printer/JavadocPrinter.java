84ffc0cba7b59c7c1c77d980cfe86658f005143c
Marco Hunsicker <marcohu@users.sourceforge.net>
2002-09-16 15:17:11 +0000

    The regexp for the Javadoc Standard tags did not work with qualified names

--- main/src/java/de/hunsicker/jalopy/printer/JavadocPrinter.java
+++ main/src/java/de/hunsicker/jalopy/printer/JavadocPrinter.java
@@ -1,32 +1,32 @@
 /*
  * Copyright (c) 2001-2002, Marco Hunsicker. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- * 1. Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
- * 
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
- *    the documentation and/or other materials provided with the 
- *    distribution. 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * 3. Neither the name of the Jalopy project nor the names of its 
- *    contributors may be used to endorse or promote products derived 
- *    from this software without specific prior written permission. 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * 3. Neither the name of the Jalopy project nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * $Id$
@@ -111,7 +111,7 @@ final class JavadocPrinter
     {
         try
         {
-            _pattern = new Perl5Compiler().compile("(?: )*(\\w*)\\s*(.*)",
+            _pattern = new Perl5Compiler().compile("(?: )*([a-zA-z0-9_.]*)\\s*(.*)",
                                                    Perl5Compiler.READ_ONLY_MASK);
         }
         catch (MalformedPatternException ex)
@@ -878,7 +878,7 @@ final class JavadocPrinter
                                NodeWriter out)
     {
         boolean needTag = false; // need @return tag?
-LOOP: 
+LOOP:
         for (AST child = node.getFirstChild();
              child != null;
              child = child.getNextSibling())
@@ -1550,10 +1550,10 @@ LOOP:
         throws IOException
     {
         AST next = EMPTY_NODE;
-ITERATION: 
+ITERATION:
         for (AST child = node; child != null; child = child.getNextSibling())
         {
-SELECTION: 
+SELECTION:
             for (;;)
             {
                 switch (child.getType())
@@ -1965,12 +1965,12 @@ SELECTION:
         if (node.getFirstChild() != null)
         {
             out.printNewline();
-ITERATION: 
+ITERATION:
             for (AST child = node.getFirstChild();
                  child != null;
                  child = child.getNextSibling())
             {
-SELECTION: 
+SELECTION:
                 for (;;)
                 {
                     switch (child.getType())
@@ -2913,7 +2913,7 @@ if (offset > -1)
     {
         StringBuffer buf = new StringBuffer(200);
         AST next = EMPTY_NODE;
-LOOP: 
+LOOP:
         for (AST child = node; child != null; child = child.getNextSibling())
         {
             switch (child.getType())
@@ -3260,7 +3260,7 @@ LOOP:
 
                 do
                 {
-MOVE_FORWARD: 
+MOVE_FORWARD:
                     while (((nextStart - lineStart) < width) &&
                            (nextStart != BreakIterator.DONE))
                     {
