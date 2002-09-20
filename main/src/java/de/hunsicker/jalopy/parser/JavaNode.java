3bf27714dff85e9031a31f8ff17792d2a4ad474d
Marco Hunsicker <marcohu@users.sourceforge.net>
2002-09-20 14:31:33 +0000

    Minor Javadoc enhancements.

--- main/src/java/de/hunsicker/jalopy/parser/JavaNode.java
+++ main/src/java/de/hunsicker/jalopy/parser/JavaNode.java
@@ -1,32 +1,32 @@
 /*
  * Copyright (c) 2001-2002, Marco Hunsicker. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- * 1. Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
- * 
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
- *    the documentation and/or other materials provided with the 
- *    distribution. 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * 3. Neither the name of the Jalopy project nor the names of its 
- *    contributors may be used to endorse or promote products derived 
- *    from this software without specific prior written permission. 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * 3. Neither the name of the Jalopy project nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * $Id$
@@ -140,10 +140,10 @@ public class JavaNode
 
 
     /**
-     * Returns the first comment before the node.
+     * Returns the first comment before this node.
      *
      * @return the first comment before the node. Returns <code>null</code> if
-     *         the node does not have a comment before.
+     *         this node does not have a comment before.
      */
     public CommonHiddenStreamToken getFirstCommentBefore()
     {
@@ -214,9 +214,9 @@ public class JavaNode
 
 
     /**
-     * Returns the number of comments that appear after the node.
+     * Returns the number of comments that appear after this node.
      *
-     * @return number of comment after the node.
+     * @return number of comment after this node.
      */
     public int getSizeCommentsAfter()
     {
@@ -248,9 +248,9 @@ public class JavaNode
 
 
     /**
-     * Returns the number of comments that appear before the node.
+     * Returns the number of comments that appear before this node.
      *
-     * @return number of comment before the node.
+     * @return number of comment before this node.
      */
     public int getSizeCommentsBefore()
     {
@@ -341,9 +341,9 @@ public class JavaNode
 
 
     /**
-     * Indicates whether the node has a Javadoc comment.
+     * Indicates whether this node has a Javadoc comment attached.
      *
-     * @return <code>true</code> if the node has a Javadoc comment.
+     * @return <code>true</code> if this node has a Javadoc comment attached.
      */
     public boolean hasJavadocComment()
     {
